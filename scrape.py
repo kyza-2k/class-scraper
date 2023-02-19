120bb3e6f7142ea223e6b8e1650b43566531c5c1
Evan Smith <evan.leigh@outlook.com>
2023-02-19 17:43:22 -0500

    feat: added headers

--- scrape.py
+++ scrape.py
@@ -17,7 +17,11 @@ with open(config_file) as f:
 url = config["url"]
 format_config = config["format"]
 
-response = requests.get(url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
 
 if response.status_code != 200:
     print('Error fetching URL:', response.status_code)
