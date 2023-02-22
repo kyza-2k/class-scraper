26bf39bc4be816f3e1dcc288212bb2919df6b117
Evan Smith <evan.leigh@outlook.com>
2023-02-22 18:18:39 -0500

    feat: adding CORS

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import json
 import os
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+CORS(app)
 
 
 def scrape_website(config):
