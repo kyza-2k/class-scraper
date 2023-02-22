ca7aa215ff9ba18089810380b489275747d4a67e
Evan Smith <evan.leigh@outlook.com>
2023-02-22 16:20:48 -0500

    fix: updated port

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ if __name__ == "__main__":
         if data is not None:
             print(json.dumps(data, indent=2))
     else:
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        app.run(debug=True, port=os.getenv("PORT", default=5000))
